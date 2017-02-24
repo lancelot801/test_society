b75ea6476c19fbf2e5588c9608d3d3a684201b50
lancelot801 <lancelot801@163.com>
2017-02-24 14:38:39 +0800

    1.添加Service和ServiceImpl的项目结构 2.新建注册页面 register.jsp 3.格式化index.html的代码 4.修改配置文件

---
+++ src/main/java/cn/joongky/service/UserLoginService.java
@@ -0,0 +1,5 @@
+package cn.joongky.service;
+
+public interface UserLoginService {
+
+}
---
+++ src/main/java/cn/joongky/service/impl/UserLoginServiceImpl.java
@@ -0,0 +1,14 @@
+package cn.joongky.service.impl;
+
+import javax.inject.Inject;
+
+import org.springframework.stereotype.Service;
+
+import cn.joongky.service.UserLoginService;
+import cn.joongky.society.dao.UserLoginMapper;
+
+@Service("userLoginService")
+public class UserLoginServiceImpl implements UserLoginService{
+	// @Inject
+	// private UserLoginMapper userloginMapper;
+}
